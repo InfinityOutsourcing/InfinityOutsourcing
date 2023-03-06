3cd9110890b77dc36cb87f4cf3a05493a6119495
aditiagrawal1612 <aditiagrawal0016@gmail.com>
2023-03-06 21:04:37 +0530

    Fixed Home Images

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -25,7 +25,7 @@ export default function Slider() {
         <Carousel.Item interval={2000}>
           <CoverImage
             className="d-block w-100 carousel-img"
-            src="https://images.unsplash.com/photo-1590212151175-e58edd96185b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNvbXB1dGVyJTIwc2V0dXB8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+            src="https://images.unsplash.com/photo-1593062096033-9a26b09da705?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
             alt="Second slide"
           />
           <Carousel.Caption>
